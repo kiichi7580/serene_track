bfd2c49598761bd4c7fcbdc54fb4ffe1db660837
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-15 13:27:32 +0900

    アプリ内のappBarを統一するためにmyAppBarを作成

---
+++ lib/component/my_appbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:serene_track/constant/colors.dart';
+
+myAppBar({
+  required String title,
+  required List<Widget>? actions,
+}) {
+  return AppBar(
+    backgroundColor: backGroundColor,
+    title: Text(
+      title,
+      style: const TextStyle(
+        color: textMainColor,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    centerTitle: false,
+    actions: actions,
+  );
+}
