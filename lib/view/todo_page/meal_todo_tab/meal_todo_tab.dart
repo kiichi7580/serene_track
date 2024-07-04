537a8fab01e6e83f273fe5088f8f49525e7755de
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-04 13:36:33 +0900

    todoページのカテゴリーごとのタブを作成

---
+++ lib/view/todo_page/meal_todo_tab/meal_todo_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MealTodoTab extends StatelessWidget {
+  const MealTodoTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('MealTodoTab'),
+    );
+  }
+}
---
+++ lib/view/todo_page/sleep_todo_tab/sleep_todo_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SleepTodoTab extends StatelessWidget {
+  const SleepTodoTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('SleepTodoTab'),
+    );
+  }
+}
