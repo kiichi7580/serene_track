eb154b66c0a0e926d29dcc1862f6bc54319b2c79
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-09 22:16:40 +0900

    デバイスの横幅がモバイル端末の場合のレイアウト画面作成

---
+++ lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+  static String get routeName => 'mobile_screen_layout';
+  static String get routeLocation => '/$routeName';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
