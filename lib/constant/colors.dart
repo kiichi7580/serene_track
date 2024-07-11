da2c9ad9dd4b2e1d8f764479f9c5db86689bc9c1
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-11 13:46:06 +0900

    カラー一覧を設定するファイルを作成

---
+++ lib/constant/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor = Color.fromRGBO(255, 255, 255, 1);
+const black26Color = Colors.black26;
+const textMainColor = Color.fromRGBO(38, 38, 38, 1);
+
+const selectedColor = Color.fromRGBO(75, 184, 71, 1);
+const unselectedColor = Color.fromRGBO(128, 128, 128, 1);
+
+// メインカラー
+const rustColor = Color.fromRGBO(183, 65, 14, 1);
+const burntOrangeColor = Color.fromRGBO(204, 85, 0, 1);
+const darkOrangeColor = Color.fromRGBO(255, 165, 0, 1);
+const sunglowColor = Color.fromRGBO(255, 203, 46, 1);
+const sandwispColor = Color.fromRGBO(244, 242, 159, 1);
+const yellowGreenColor = Color.fromRGBO(177, 225, 122, 1);
+const mantisColor = Color.fromRGBO(127, 209, 92, 1);
+const appleColor = Color.fromRGBO(75, 184, 71, 1);
+
+// グレイ系
+const greyColor = Colors.grey;
+Color lightGreyColor = Colors.grey[400]!;
+
+// リンクの色
+const linkBlue = Color.fromRGBO(27, 149, 224, 1);
+
+// deleteカラー
+const deleteColor = Color.fromRGBO(255, 102, 102, 1.0);
+
+// reactionカラー
+const yesReactionColor = Color.fromRGBO(0, 255, 0, 1.0);
+const noReactionColor = Color.fromRGBO(255, 0, 0, 1.0);
