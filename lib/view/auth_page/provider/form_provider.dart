4efafcfa31d5eedaeb76358d71e6145fd629d0e6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-11 16:08:09 +0900

    FormStatusの値の状態を管理し、FormStatus.validが返されたら、フォーム内容を送信できるように制御

---
+++ lib/view/auth_page/provider/form_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/model/enum/form_logic.dart';
+import 'package:serene_track/model/enum/form_state.dart';
+
+final formProvider =
+    NotifierProvider<FormNotifier, FormStatus>(FormNotifier.new);
+
+class FormNotifier extends Notifier<FormStatus> {
+  @override
+  build() {
+    return FormStatus.initial;
+  }
+
+  late String passwordText = '';
+  final FormLogic _formLogic = FormLogic();
+
+  FormStatus validateForm(GlobalKey<FormState> formKey) {
+    if (formKey.currentState?.validate() == true) {
+      state = FormStatus.valid;
+      return state;
+    } else {
+      state = FormStatus.invalid;
+      return state;
+    }
+  }
+
+  void getConfirmationPassword(String? value) {
+    passwordText = value!;
+  }
+
+  String? emailrValidator(String? value) => _formLogic.emailValidator(value);
+
+  String? passwordValidator(String? value) =>
+      _formLogic.passwordValidator(value);
+
+  String? confirmationPasswordValidator(String? value) =>
+      _formLogic.confirmationPasswordValidator(
+        value,
+        passwordText: passwordText,
+      );
+}
