f71ce506bd8d80a540ea9ab47f878f6ec3ac71fd
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-11 13:47:25 +0900

    直書きをなるべく減らすためテキストソースをまとめておくファイルを作成

---
+++ lib/constant/text_source.dart
@@ -0,0 +1,81 @@
+// レスポンス
+const userNull = 'ユーザー情報の取得に失敗しました。\n通信環境を確認して、再度お試しください。';
+const userNotNull = 'ユーザー情報の取得に成功しました。';
+const successRes = 'success';
+const successAdd = '追加が完了しました。';
+const successUpDate = '更新が完了しました。';
+const successDelete = '削除が完了しました。';
+
+const failureRes = 'failure';
+const failureAdd = '追加できませんでした。もう一度やり直してください。';
+const failureUpDate = '更新できませんでした。もう一度やり直してください。';
+const failureDelete = '削除できませんでした。もう一度やり直してください。';
+
+// CRUDサイクル
+
+// Create
+const addTx = '追加';
+const createTx = '作成';
+const saveTx = '保存';
+
+// Update
+const upDateTx = '更新';
+
+const editTx = '編集';
+
+// Delete
+const deleteTx = '削除';
+
+// 新規登録
+const successSignUp = '新規登録に成功しました！';
+const failureSignUp = '問題が発生しました。もう一度、新規登録をしてください。';
+
+// 新規登録時のバリデーション
+const pleaseEnterEmail = 'メースアドレスを入力してください';
+const pleaseEnterPassword = 'パスワードを入力してください';
+const wrongPassword = 'パスワードが正しくありません';
+
+// サインイン
+const successSignIn = 'サインインに成功しました！';
+const failureSignIn = '問題が発生しました。もう一度、サインインしてください。';
+
+// パスワードを忘れた場合
+const forgotYourPassword = 'パスワードを忘れましたか？';
+const userNotFound = 'user-not-found';
+const userNotFoundMessage = 'ユーザーが見つかりません';
+const successSendEmail = 'パスワード再設定手続きのためのメールを送信しました。';
+const successResendEmail = 'メールを再送信しました。メールボックスをご確認ください。';
+const successResetPassword = 'パスワードの変更が完了しました。';
+
+// Googleでサインアップ・ログイン
+const signUpOnGoogle = 'Googleで新規登録';
+const loginOnGoogle = 'Googleでログイン';
+
+// 入力バリデーション
+const validationRes = '未入力の項目があります';
+
+const emptyOrNullEmailMessage = 'メールアドレスを入力してください。';
+const invalidEmailMessage = 'メールアドレスが有効ではありません';
+const emailAlreadyInUseMessage = 'そのメールアドレスを持つアカウントは既に存在しています';
+
+const emptyOrNullPasswordMessage = 'パスワードを入力してください';
+const weakPasswordMessage = '英語の大文字、小文字、数字を含む8文字以上を入力してください';
+
+const emptyOrNullConfirmationPasswordMessage = '確認用パスワードを入力してください';
+const correctPasswordMessage = '正しいパスワードを入力してください';
+
+// ボトムナビゲーションバー
+const taskTx = 'タスク';
+const montringTx = 'モニタリング';
+const accountTx = 'アカウント';
+
+// タスク関連
+const titleTx = 'タイトル';
+const categoryTx = 'カテゴリー';
+const descriptionTx = '内容';
+const notificationSettingTx = '通知設定';
+
+const titleFieldEmpty = 'タイトルが入力されていません';
+const categoryFieldEmpty = 'カテゴリーが入力されていません';
+const descriptionFieldEmpty = '内容が入力されていません';
+const notificationSettingFieldEmpty = '通知設定が入力されていません';
