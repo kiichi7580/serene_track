c49756d79841de568d285e2bebeaf4704b69703a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-15 13:06:55 +0900

    StatelessWidgetからConsumerWidgetに変更、exerciseTodoList関連を別のファイルで状態管理するようにした

--- lib/view/todo_page/exercise_todo_tab/exercise_todo_tab.dart
+++ lib/view/todo_page/exercise_todo_tab/exercise_todo_tab.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/model/todo.dart';
+import 'package:serene_track/view/todo_page/exercise_todo_tab/provider/exercise_todo_tab_notifier.dart';
+import 'package:serene_track/view/todo_page/widgets/custom_checkbox_tile.dart';
 
-class ExerciseTodoTab extends StatelessWidget {
+class ExerciseTodoTab extends ConsumerWidget {
   const ExerciseTodoTab({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('ExerciseTodoTab'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final exerciseTodoList = ref.read(exerciseTodoTabProvider).exerciseTodoList;
+    final selectedItemList = ref.watch(exerciseTodoTabProvider).isSelectedList;
+    final checkedList = ref.watch(exerciseTodoTabProvider).checkedList;
+
+    return Scaffold(
+      backgroundColor: yellowGreenColor,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: exerciseTodoList.length,
+        itemBuilder: (context, index) {
+          Todo exerciseTodo = exerciseTodoList[index];
+
+          return CustomCheckboxTile(
+            todo: exerciseTodo,
+            index: index,
+            value: checkedList[index],
+            fillColor: yellowGreenColor,
+            onTap: () {
+              ref
+                  .read(exerciseTodoTabProvider.notifier)
+                  .changeSelectedItemList(index);
+            },
+            onChanged: (value) {
+              ref
+                  .watch(exerciseTodoTabProvider.notifier)
+                  .changeCheckedList(index, value!);
+            },
+            selectedItemList: selectedItemList,
+          );
+        },
+      ),
     );
   }
 }
