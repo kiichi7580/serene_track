2601fd69e8288049b9be217bb5de9443927c2e87
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-15 13:07:20 +0900

    StatelessWidgetからConsumerWidgetに変更、sleepTodoList関連を別のファイルで状態管理するようにした

--- lib/view/todo_page/sleep_todo_tab/sleep_todo_tab.dart
+++ lib/view/todo_page/sleep_todo_tab/sleep_todo_tab.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/model/todo.dart';
+import 'package:serene_track/view/todo_page/sleep_todo_tab/provider/sleep_todo_tab_notifier.dart';
+import 'package:serene_track/view/todo_page/widgets/custom_checkbox_tile.dart';
 
-class SleepTodoTab extends StatelessWidget {
+class SleepTodoTab extends ConsumerWidget {
   const SleepTodoTab({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('SleepTodoTab'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sleepTodoList = ref.watch(sleepTodoTabProvider).sleepTodoList;
+    final selectedItemList = ref.watch(sleepTodoTabProvider).isSelectedList;
+    final checkedList = ref.watch(sleepTodoTabProvider).checkedList;
+
+    return Scaffold(
+      backgroundColor: mantisColor,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: sleepTodoList.length,
+        itemBuilder: (context, index) {
+          Todo sleepTodo = sleepTodoList[index];
+
+          return CustomCheckboxTile(
+            todo: sleepTodo,
+            index: index,
+            value: checkedList[index],
+            fillColor: sandwispColor,
+            onTap: () {
+              ref
+                  .read(sleepTodoTabProvider.notifier)
+                  .changeSelectedItemList(index);
+            },
+            onChanged: (value) {
+              ref
+                  .watch(sleepTodoTabProvider.notifier)
+                  .changeCheckedList(index, value!);
+            },
+            selectedItemList: selectedItemList,
+          );
+        },
+      ),
     );
   }
 }
