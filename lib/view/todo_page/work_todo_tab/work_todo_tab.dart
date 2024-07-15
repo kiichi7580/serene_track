b8b81d3c18b1fd3011e1a29135bf2cc66dc6d819
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-15 13:08:01 +0900

    StatelessWidgetからConsumerWidgetに変更、workTodoList関連を別のファイルで状態管理するようにした

--- lib/view/todo_page/work_todo_tab/work_todo_tab.dart
+++ lib/view/todo_page/work_todo_tab/work_todo_tab.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/model/todo.dart';
+import 'package:serene_track/view/todo_page/widgets/custom_checkbox_tile.dart';
+import 'package:serene_track/view/todo_page/work_todo_tab/provider/work_todo_tab_notifier.dart';
 
-class WorkTodoTab extends StatelessWidget {
+class WorkTodoTab extends ConsumerWidget {
   const WorkTodoTab({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Container(
-      child: Text('WorkTodoTab'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final workTodoList = ref.watch(workTodoTabProvider).workTodoList;
+    final selectedItemList = ref.watch(workTodoTabProvider).isSelectedList;
+    final checkedList = ref.watch(workTodoTabProvider).checkedList;
+
+    return Scaffold(
+      backgroundColor: appleColor,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: workTodoList.length,
+        itemBuilder: (context, index) {
+          Todo workTodo = workTodoList[index];
+
+          return CustomCheckboxTile(
+            todo: workTodo,
+            index: index,
+            value: checkedList[index],
+            fillColor: appleColor,
+            onTap: () {
+              ref
+                  .read(workTodoTabProvider.notifier)
+                  .changeSelectedItemList(index);
+            },
+            onChanged: (value) {
+              ref
+                  .watch(workTodoTabProvider.notifier)
+                  .changeCheckedList(index, value!);
+            },
+            selectedItemList: selectedItemList,
+          );
+        },
+      ),
     );
   }
 }
