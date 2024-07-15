1a6ae1651e7a8a4ee077686ffa7a205380fea4c1
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-15 13:06:22 +0900

    StatelessWidgetからConsumerWidgetに変更、allTodoList関連を別のファイルで状態管理するようにした

--- lib/view/todo_page/all_todo_tab/all_todo_tab.dart
+++ lib/view/todo_page/all_todo_tab/all_todo_tab.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/model/todo.dart';
+import 'package:serene_track/view/todo_page/all_todo_tab/provider/all_todo_tab_notifier.dart';
+import 'package:serene_track/view/todo_page/widgets/custom_checkbox_tile.dart';
 
-class AllTodoTab extends StatelessWidget {
+class AllTodoTab extends ConsumerWidget {
   const AllTodoTab({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allTodoList = ref.watch(allTodoTabProvider).allTodoList;
+    final selectedItemList = ref.watch(allTodoTabProvider).isSelectedList;
+    final checkedList = ref.watch(allTodoTabProvider).checkedList;
+
+    return Scaffold(
+      backgroundColor: sandwispColor,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: allTodoList.length,
+        itemBuilder: (context, index) {
+          Todo allTodo = allTodoList[index];
+
+          return CustomCheckboxTile(
+            todo: allTodo,
+            index: index,
+            value: checkedList[index],
+            fillColor: sandwispColor,
+            onTap: () {
+              ref
+                  .read(allTodoTabProvider.notifier)
+                  .changeSelectedItemList(index);
+            },
+            onChanged: (value) {
+              ref
+                  .watch(allTodoTabProvider.notifier)
+                  .changeCheckedList(index, value!);
+            },
+            selectedItemList: selectedItemList,
+          );
+        },
+      ),
+    );
   }
 }
