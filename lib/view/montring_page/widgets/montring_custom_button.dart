a2fdb235f0af187ff439b93fd8c34d370ed0857a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-16 23:08:37 +0900

    モニタリング画面でタイマーの制御をするボタンMontringCustomButtonを作成、montringPageProviderで状態管理

---
+++ lib/view/montring_page/widgets/montring_custom_button.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/view/montring_page/provider/montring_page_notifier.dart';
+import 'package:serene_track/view/montring_page/widgets/show_break_promotion_dialog.dart';
+
+class MontringCustomButton extends ConsumerWidget {
+  const MontringCustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isRunning = ref.watch(montringPageProvider.notifier).getIsRunning;
+    final dynamicSeconds = ref.watch(montringPageProvider).dynamicSeconds;
+    final timer = ref.watch(montringPageProvider).timer;
+
+    return isRunning && dynamicSeconds != 0
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton(
+                height: 70,
+                width: 150,
+                text: isRunning ? 'ストップ' : 'スタート',
+                onPressed: () {
+                  if (isRunning) {
+                    ref.read(montringPageProvider.notifier).stopTimer();
+                  } else {
+                    ref.read(montringPageProvider.notifier).startTimer();
+                  }
+                },
+              ),
+              const SizedBox(width: 12),
+              buildButton(
+                height: 70,
+                width: 150,
+                text: 'リセット',
+                onPressed: () {
+                  ref.read(montringPageProvider.notifier).resetTimer();
+                },
+              ),
+            ],
+          )
+        : dynamicSeconds != 0
+            ? buildButton(
+                height: 70,
+                width: 230,
+                text: 'スタート',
+                onPressed: () async {
+                  ref.read(montringPageProvider.notifier).startTimer();
+                },
+              )
+            : buildButton(
+                height: 70,
+                width: 230,
+                text: 'タイマーを止める',
+                onPressed: () async {
+                  ref.read(montringPageProvider.notifier).stopTimer();
+                  ref.watch(montringPageProvider.notifier).resetTimer();
+                  final response = await showBreakPromotionDialog(
+                    context,
+                  );
+                  if (response == null || response == false) {
+                    return;
+                  }
+                },
+              );
+  }
+
+  Widget buildButton({
+    required double? height,
+    required double? width,
+    required String text,
+    required void Function()? onPressed,
+    Color? textColor = navyBlueColor,
+  }) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backGroundColor,
+          side: const BorderSide(
+            color: navyBlueColor,
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
