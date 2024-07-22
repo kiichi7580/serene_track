a6265d2e446aa675f7f4132f4eaf900643e27b7f
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-22 14:00:01 +0900

    アカウント画面を更新、上部にユーザー情報を表示、下部はタブで表示分けするようにし、より詳細なユーザー情報を表示する予定

--- lib/view/account_page/account_page.dart
+++ lib/view/account_page/account_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:serene_track/components/my_appbar.dart';
 import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/constant/text_source.dart';
+import 'package:serene_track/view/account_page/components/account_header.dart';
+import 'package:serene_track/view/account_page/components/account_tab.dart';
+import 'package:serene_track/view/account_page/components/account_tabs_contents.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
@@ -9,21 +12,26 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: myAppBar(
-        title: 'ユーザー名',
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.more_horiz,
-            ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: backGroundColor,
+        appBar: AppBar(
+          title: const AccountHeader(),
+          toolbarHeight: 240,
+          backgroundColor: backGroundColor,
+          bottom: const TabBar(
+            padding: EdgeInsets.only(top: 16, right: 16, left: 16),
+            indicatorColor: appleColor,
+            indicatorPadding: EdgeInsets.symmetric(horizontal: 8),
+            tabs: [
+              AccountTab(tabName: taskTx),
+              AccountTab(tabName: stepsTx),
+              AccountTab(tabName: sleepTx),
+            ],
           ),
-        ],
-      ),
-      backgroundColor: backGroundColor,
-      body: const Center(
-        child: Text('アカウント'),
+        ),
+        body: const AccountTabsContents(),
       ),
     );
   }
