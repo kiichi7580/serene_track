3a988c888ca251bd3cac236981d33d64b855eb81
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-24 04:05:17 +0900

    ヘルスケア連携解除時の注意を表示するダイアログ作成

---
+++ lib/view/health_care_page/components/cancellation_of_integration_with_health_care_app_dialog.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/components/custom_dialog.dart';
+import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/constant/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> showCancellationOfIntegrationHealthCareAppDialog(
+    BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: _buildCancellationOfIntegrationHealthCareAppDialog,
+  );
+}
+
+Widget _buildCancellationOfIntegrationHealthCareAppDialog(
+    BuildContext context) {
+  return Consumer(
+    builder: (context, ref, child) {
+      return CustomDialog(
+        title: '連携解除の注意',
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 18, vertical: 40),
+        content: const Text(
+          '　ヘルスケアアプリとの連携を解除するには、Appleのヘルスケアアプリで、設定を変更してください。ヘルスケアアプリを開き、下の「共有」アイコンを選択後、アプリおよびサービスを選択し、当アプリを探してアクセスを解除してください。',
+          style: TextStyle(fontSize: 15),
+          textAlign: TextAlign.start,
+        ),
+        actions: [
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              foregroundColor: textMainColor,
+              backgroundColor: backGroundColor,
+            ),
+            child: const Text('キャンセル'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              foregroundColor: linkBlue,
+              backgroundColor: backGroundColor,
+            ),
+            child: const Text('ヘルスケアアプリを開く'),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              if (Platform.isIOS) {
+                final url = Uri.parse(kHealthCareAppUrl);
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              }
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
