8333422e4c800add3b0604ce607560a10c26c213
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-29 16:12:38 +0900

    ログアウトの項目を追加

--- lib/view/account_setting_page/account_setting_page.dart
+++ lib/view/account_setting_page/account_setting_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:serene_track/components/my_appbar.dart';
 import 'package:serene_track/constant/colors.dart';
 import 'package:serene_track/constant/themes/text_styles.dart';
+import 'package:serene_track/controllers/global/user_notifier.dart';
+import 'package:serene_track/view/account_setting_page/components/show_log_out_dialog.dart';
+import 'package:serene_track/view/auth_page/sign_in_page/sign_in_page.dart';
 import 'package:serene_track/view/health_care_page/health_care_app_integration_page.dart';
 
-class AccountSettingPage extends StatelessWidget {
+class AccountSettingPage extends ConsumerWidget {
   const AccountSettingPage({super.key});
   static String get routeName => 'account_setting';
   static String get routeLocation => '/$routeName';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: backGroundColor,
       appBar: myAppBar(
@@ -24,10 +28,11 @@ class AccountSettingPage extends StatelessWidget {
           settingItem(
             title: 'ヘルスケアと連携する',
             iconData: Icons.favorite,
-            onPressed: () {
+            onTap: () {
               context.push(HealthCareAppIntegrationPage.routeLocation);
             },
           ),
+          logOut(context: context, ref: ref),
         ],
       ),
     );
@@ -36,16 +41,38 @@ class AccountSettingPage extends StatelessWidget {
   Widget settingItem({
     required String title,
     required IconData? iconData,
-    required void Function()? onPressed,
+    required void Function()? onTap,
   }) {
     return ListTile(
       contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
       leading: Icon(iconData),
       title: Text(title, style: TextStyles.caption),
-      trailing: IconButton(
-        onPressed: onPressed,
-        icon: const Icon(Icons.arrow_forward_ios),
+      trailing: const Icon(Icons.arrow_forward_ios),
+      onTap: onTap,
+    );
+  }
+
+  Widget logOut({
+    required BuildContext context,
+    required WidgetRef ref,
+  }) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+      leading: const Icon(Icons.logout, color: noReactionColor),
+      title: Text(
+        'ログアウトする',
+        style: TextStyles.caption.copyWith(color: noReactionColor),
       ),
+      onTap: () async {
+        final response = await showLogOutDiolog(
+          context,
+        );
+        if (response == null || response == false) {
+          return;
+        }
+        await ref.read(userProvider.notifier).logOut();
+        context.go(SignInPage.routeLocation);
+      },
     );
   }
 }
