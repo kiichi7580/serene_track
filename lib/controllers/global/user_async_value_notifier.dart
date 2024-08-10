c453af98e03c5b69f4cb0027877787e6a0fa36be
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-08-10 21:10:20 +0900

    ユーザー認証関連の処理をuserProvider内で実行するようにしたので、userAsyncValueProviderを削除

--- lib/controllers/global/user_async_value_notifier.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:serene_track/model/src/user.dart';
-import 'package:serene_track/view/auth_page/provider/auth_notifier.dart';
-
-final userAsyncValueProvider = FutureProvider<User?>((ref) async {
-  final User? user = await ref.read(authProvider.notifier).getUser();
-  return user;
-});
