f41d38bbe5df581bd2f27e9fbbfc7dc8e9a5316b
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-08-13 19:28:52 +0900

    formKeyを設定し、未入力の項目があったらバリデーションされるようにした

--- lib/view/account_edit_page/edit_account_page.dart
+++ lib/view/account_edit_page/edit_account_page.dart
@@ -1,53 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:serene_track/components/loading_button.dart';
 import 'package:serene_track/components/my_appbar.dart';
 import 'package:serene_track/components/show_snack_bar.dart';
 import 'package:serene_track/constant/colors.dart';
 import 'package:serene_track/constant/text_source.dart';
 import 'package:serene_track/controllers/global/user_notifier.dart';
 import 'package:serene_track/gen/assets.gen.dart';
+import 'package:serene_track/model/enum/form_state.dart';
 import 'package:serene_track/view/account_edit_page/components/account_text_field.dart';
 import 'package:serene_track/components/custom_button.dart';
 import 'package:serene_track/view/account_edit_page/provider/account_text_field_notifier.dart';
+import 'package:serene_track/view/account_edit_page/provider/form_key_for_edit_account_notifier.dart';
 import 'package:serene_track/view/account_page/account_page.dart';
+import 'package:serene_track/view/auth_page/provider/form_provider.dart';
 
 class EditAccountPage extends ConsumerWidget {
   const EditAccountPage({super.key});
   static String get routeName => 'edit_account';
   static String get routeLocation => '/$routeName';
 
-  Future<void> updateUser({
-    required BuildContext context,
+  Future<String> updateUser({
     required WidgetRef ref,
     required String name,
     required String shortTermGoal,
     required String longTermGoal,
   }) async {
-    if (name.isNotEmpty &&
-        shortTermGoal.isNotEmpty &&
-        longTermGoal.isNotEmpty) {
-      String res = await ref.read(userProvider.notifier).updateUser(
-            name: name,
-            shortTermGoal: shortTermGoal,
-            longTermGoal: longTermGoal,
-          );
-      if (res == successRes) {
-        context.go(AccountPage.routeLocation);
-      } else {
-        showSnackBar(res, context, backgroundColor: noReactionColor);
-      }
+    String res = failureUpDate;
+    res = await ref.read(userProvider.notifier).updateUser(
+          name: name,
+          shortTermGoal: shortTermGoal,
+          longTermGoal: longTermGoal,
+        );
+    if (res == successRes) {
+      res = successUpDate;
     }
+    return res;
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider.select((value) => value.user));
+    final isLoading =
+        ref.watch(userProvider.select((value) => value.isLoading));
     final nameController = ref.watch(nameControllerNotifierProvider(user.name));
     final shortTermGoalController =
         ref.watch(shortTermGorlControllerNotifierProvider(user.shortTermGoal));
     final longTermGoalController =
         ref.watch(longTermGorlControllerNotifierProvider(user.longTermGoal));
+    final formController = ref.read(formProvider.notifier);
+    final formKey = ref.read(formKeyForEditAccountNotifierProvider);
     return Scaffold(
       backgroundColor: backGroundColor,
       appBar: myAppBar(title: 'アカウントを編集'),
@@ -61,55 +64,78 @@ class EditAccountPage extends ConsumerWidget {
           decoration: const BoxDecoration(
             color: backGroundColor,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.width * 0.04,
-              ),
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: iconImage(user.photoUrl),
-                  radius: 64,
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.04,
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.06,
-              ),
-              AccountTextField(
-                caption: userName,
-                controller: nameController,
-                keyboardType: TextInputType.text,
-                maxLength: 45,
-              ),
-              const SizedBox(height: 4),
-              AccountTextField(
-                caption: shortTermGorlTx,
-                controller: shortTermGoalController,
-                keyboardType: TextInputType.text,
-                maxLength: 100,
-              ),
-              const SizedBox(height: 4),
-              AccountTextField(
-                caption: longTermGorlTx,
-                controller: longTermGoalController,
-                keyboardType: TextInputType.text,
-                maxLength: 100,
-              ),
-              const SizedBox(height: 16),
-              CustomButton(
-                buttonText: '更新する',
-                onTap: () async {
-                  await updateUser(
-                    context: context,
-                    ref: ref,
-                    name: nameController.text,
-                    shortTermGoal: shortTermGoalController.text,
-                    longTermGoal: longTermGoalController.text,
-                  );
-                },
-              ),
-            ],
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: iconImage(user.photoUrl),
+                    radius: 64,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                ),
+                AccountTextField(
+                  caption: userName,
+                  controller: nameController,
+                  keyboardType: TextInputType.text,
+                  maxLength: 45,
+                  validator: formController.basicValidator,
+                ),
+                const SizedBox(height: 4),
+                AccountTextField(
+                  caption: shortTermGorlTx,
+                  controller: shortTermGoalController,
+                  keyboardType: TextInputType.text,
+                  maxLength: 100,
+                  validator: formController.basicValidator,
+                ),
+                const SizedBox(height: 4),
+                AccountTextField(
+                  caption: longTermGorlTx,
+                  controller: longTermGoalController,
+                  keyboardType: TextInputType.text,
+                  maxLength: 100,
+                  validator: formController.basicValidator,
+                ),
+                const SizedBox(height: 16),
+                isLoading
+                    ? const LoadingButton()
+                    : CustomButton(
+                        buttonText: '更新する',
+                        onTap: () async {
+                          FormStatus formStatus =
+                              formController.validateForm(formKey);
+                          if (formStatus == FormStatus.valid) {
+                            String res = await updateUser(
+                              ref: ref,
+                              name: nameController.text,
+                              shortTermGoal: shortTermGoalController.text,
+                              longTermGoal: longTermGoalController.text,
+                            );
+                            if (res == successUpDate) {
+                              if (!context.mounted) return;
+                              context.go(AccountPage.routeLocation);
+                              showSnackBar(res, context);
+                            } else {
+                              if (!context.mounted) return;
+                              showSnackBar(
+                                res,
+                                context,
+                                backgroundColor: noReactionColor,
+                              );
+                            }
+                          }
+                        },
+                      ),
+              ],
+            ),
           ),
         ),
       ),
