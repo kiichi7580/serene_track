fee7e2c90493a9fb65c1389ba1c6f0f5f7cdaf3c
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-08-13 22:30:20 +0900

    画像をポップアップするダイアログ作成

---
+++ lib/components/image_dialog.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:serene_track/gen/assets.gen.dart';
+
+Future<void> imageDialog({
+  required BuildContext context,
+  required String imageUrl,
+}) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        content: imageUrl == ''
+            ? Image.asset(
+                Assets.images.icons.sereneTrackIconBlack.path,
+                fit: BoxFit.cover,
+              )
+            : Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+      );
+    },
+  );
+}
