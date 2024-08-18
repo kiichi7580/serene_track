a0753d1d8ccd653e3d24e15fb6a9adffe21903a0
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-08-19 00:59:40 +0900

    TodoRepositoryのプロバイダーtodoRepositoryProviderを作成、TodoのCRUDの処理を実装

---
+++ lib/infrastructure/todo/todo_repository_impl.dart
@@ -0,0 +1,116 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serene_track/controllers/global/dio_notifier.dart';
+import 'package:serene_track/model/enum/result.dart';
+import 'package:serene_track/model/src/todo.dart';
+import 'package:serene_track/repository/todo/todo_repository.dart';
+
+final todoRepositoryProvider =
+    Provider<TodoRepository>((ref) => TodoRepositoryImpl(ref));
+
+class TodoRepositoryImpl implements TodoRepository {
+  TodoRepositoryImpl(ProviderRef<TodoRepository> ref)
+      : _dio = ref.watch(dioProvider);
+
+  final Dio _dio;
+
+  @override
+  Future<Result<Todo?>> createTodo({
+    required String accessToken,
+    required String tokenType,
+    required FormData formData,
+  }) {
+    return Result.guardFuture(() async {
+      await Future.delayed(const Duration(seconds: 1));
+
+      final response = await _dio.post(
+        '/todo/',
+        options: Options(
+          headers: {'Authorization': '$tokenType $accessToken'},
+        ),
+        data: formData,
+      );
+      if (response.statusCode == 204) {
+        return null;
+      } else if (response.data != null) {
+        Todo todo = Todo.fromJson(response.data);
+        return todo;
+      } else {
+        throw Exception('Unexpected response format');
+      }
+    });
+  }
+
+  @override
+  Future<Result<List<Todo>?>> fetchTodo({
+    required String accessToken,
+    required String tokenType,
+  }) {
+    return Result.guardFuture(() async {
+      await Future.delayed(const Duration(seconds: 1));
+
+      final response = await _dio.get('/todo/',
+          options: Options(
+            headers: {'Authorization': '$tokenType $accessToken'},
+          ));
+      if (response.data.isEmpty) {
+        return [];
+      }
+      final todos =
+          List<Todo>.from(response.data.map((todo) => Todo.fromJson(todo)));
+      return todos;
+    });
+  }
+
+  @override
+  Future<Result<Todo>> updateTodo({
+    required String accessToken,
+    required String tokenType,
+    required Todo todo,
+    required FormData formData,
+  }) {
+    return Result.guardFuture(() async {
+      await Future.delayed(const Duration(seconds: 1));
+
+      final response = await _dio.put(
+        '/todo/${todo.id}',
+        options: Options(
+          headers: {'Authorization': '$tokenType $accessToken'},
+        ),
+        data: formData,
+      );
+
+      if (response.statusCode == 204) {
+        return todo;
+      } else if (response.data != null) {
+        todo = Todo.fromJson(response.data);
+        return todo;
+      } else {
+        throw Exception('Unexpected response format');
+      }
+    });
+  }
+
+  @override
+  Future<Result<Todo>> deleteTodo({
+    required String accessToken,
+    required String tokenType,
+    required Todo todo,
+  }) {
+    return Result.guardFuture(() async {
+      await Future.delayed(const Duration(seconds: 1));
+
+      final response = await _dio.delete(
+        '/todo/${todo.id}',
+        options: Options(
+          headers: {'Authorization': '$tokenType $accessToken'},
+        ),
+      );
+      if (response.statusCode == 204) {
+        return todo;
+      } else {
+        throw Exception('Unexpected response format');
+      }
+    });
+  }
+}
