e9f5e65be8eda22143a4f41b4a005d9d9c9bae02
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-08-19 01:07:39 +0900

    Slidableパッケージを使用し、タスク削除の処理を実装、ローカルにあるデフォルトのタスクは一旦コメントアウト

--- lib/view/todo_page/exercise_todo_tab/exercise_todo_tab.dart
+++ lib/view/todo_page/exercise_todo_tab/exercise_todo_tab.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:serene_track/components/delete_confirmation_dialog.dart';
+import 'package:serene_track/components/show_snack_bar.dart';
 import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/constant/text_source.dart';
+import 'package:serene_track/controllers/global/todo_notifier.dart';
 import 'package:serene_track/model/src/todo.dart';
 import 'package:serene_track/view/todo_page/exercise_todo_tab/provider/exercise_todo_tab_notifier.dart';
 import 'package:serene_track/view/todo_page/components/custom_checkbox_tile.dart';
@@ -10,38 +15,97 @@ class ExerciseTodoTab extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final exerciseTodoList = ref.watch(
-        exerciseTodoTabProvider.select((value) => value.exerciseTodoList));
+    // final exerciseTodoList = ref.watch(
+    //     exerciseTodoTabProvider.select((value) => value.exerciseTodoList));
+    final exerciseTodoList = ref
+        .watch(todoProvider.select((value) => value.todos))
+        .where((value) => value.categoryId == exerciseTx)
+        .toList();
     final selectedItemList = ref
         .watch(exerciseTodoTabProvider.select((value) => value.isSelectedList));
     final checkedList =
         ref.watch(exerciseTodoTabProvider.select((value) => value.checkedList));
+    final isLoading =
+        ref.watch(todoProvider.select((value) => value.isLoading));
 
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      itemCount: exerciseTodoList.length,
-      itemBuilder: (context, index) {
-        Todo exerciseTodo = exerciseTodoList[index];
+    return isLoading
+        ? const SizedBox(
+            height: 200,
+            child: Center(
+              child: CircularProgressIndicator(
+                color: lightSkyBlueColor,
+                strokeWidth: 3,
+              ),
+            ),
+          )
+        : ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            itemCount: exerciseTodoList.length,
+            itemBuilder: (context, index) {
+              Todo todo = exerciseTodoList[index];
 
-        return CustomCheckboxTile(
-          todo: exerciseTodo,
-          index: index,
-          value: checkedList[index],
-          fillColor: yellowGreenColor,
-          onTap: () {
-            ref
-                .read(exerciseTodoTabProvider.notifier)
-                .changeSelectedItemList(index);
-          },
-          onChanged: (value) {
-            ref
-                .watch(exerciseTodoTabProvider.notifier)
-                .changeCheckedList(index, value!);
-          },
-          selectedItemList: selectedItemList,
-        );
-      },
-    );
+              return Slidable(
+                key: ValueKey(index),
+                endActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (context) async {
+                        ref.read(todoProvider.notifier).setSelectedTodo(todo);
+                        String res =
+                            await deleteTodo(context: context, ref: ref);
+                        if (res == successDelete) {
+                          if (!context.mounted) return;
+                          showSnackBar(res, context);
+                        }
+                        if (!context.mounted) return;
+                        showSnackBar(
+                          res,
+                          context,
+                          backgroundColor: noReactionColor,
+                        );
+                      },
+                      backgroundColor: deleteColor,
+                      foregroundColor: backGroundColor,
+                      icon: Icons.delete,
+                    ),
+                  ],
+                ),
+                child: CustomCheckboxTile(
+                  // todo: todo,
+                  todos: exerciseTodoList,
+                  index: index,
+                  value: checkedList[index],
+                  fillColor: sandwispColor,
+                  onTap: () {
+                    ref
+                        .read(exerciseTodoTabProvider.notifier)
+                        .changeSelectedItemList(index);
+                  },
+                  onChanged: (value) {
+                    ref
+                        .watch(exerciseTodoTabProvider.notifier)
+                        .changeCheckedList(index, value!);
+                  },
+                  selectedItemList: selectedItemList,
+                ),
+              );
+            },
+          );
+  }
+
+  Future<String> deleteTodo({
+    required BuildContext context,
+    required WidgetRef ref,
+  }) async {
+    String res = failureDelete;
+    final response = await showDeleteConfirmationDialog(context);
+    if (response == null || response == false) {
+      return res;
+    }
+    res = await ref.read(todoProvider.notifier).deleteTodo();
+    if (res == successRes) res = successDelete;
+    return res;
   }
 }
