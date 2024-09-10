ab9eae33bd4d957ccd294a6f822efab49b74c2f8
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-09-10 18:12:32 +0900

    fix: ファイルの配置場所を変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:serene_track/router/app_router.dart';
-import 'package:serene_track/utils/local_notification_setting.dart';
+import 'package:serene_track/utils/notification/local_notification_setting.dart';
 import 'package:serene_track/utils/preferences_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
