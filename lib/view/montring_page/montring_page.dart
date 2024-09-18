1e9d6a4286659da86dd8635db54f83715e7bb42c
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-09-18 09:10:29 +0900

    feat: 休憩が達成された場合、それを紙吹雪で祝福する機能を追加

--- lib/view/montring_page/montring_page.dart
+++ lib/view/montring_page/montring_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:serene_track/components/my_appbar.dart';
 import 'package:serene_track/constant/colors.dart';
+import 'package:serene_track/view/montring_page/components/custom_confetti_widget.dart';
 import 'package:serene_track/view/montring_page/provider/montring_page_notifier.dart';
 import 'package:serene_track/view/montring_page/components/montring_custom_button.dart';
 import 'package:serene_track/view/montring_page/components/montring_timer.dart';
+import 'package:serene_track/view/montring_page/provider/show_break_promotion_dialog_notifier.dart';
 
 class MontringPage extends ConsumerWidget {
   const MontringPage({super.key});
@@ -15,21 +17,33 @@ class MontringPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isRunning =
         ref.watch(montringPageProvider.select((value) => value.isRunning));
+    final isAllChecked = ref.watch(
+        showBreakPromotionDialogProvider.select((value) => value.isAllChecked));
+    final dynamicSeconds =
+        ref.watch(montringPageProvider.select((value) => value.dynamicSeconds));
+    final response = ref.watch(
+        showBreakPromotionDialogProvider.select((value) => value.response));
 
     return Scaffold(
       backgroundColor: backGroundColor,
       appBar: myAppBar(title: 'モニタリング'),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: backGroundColor,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(flex: 1, child: Container()),
-            Container(
+      body: Stack(
+        children: [
+          if (isAllChecked && response) ...[
+            Positioned(
+              top: -80,
+              left: 170,
+              child: Container(
+                padding: const EdgeInsets.all(30),
+                alignment: Alignment.topCenter,
+                child: const CustomConfettiWidget(),
+              ),
+            ),
+          ],
+          Positioned(
+            top: 90,
+            left: 140,
+            child: Container(
               alignment: Alignment.center,
               height: 60,
               child: Padding(
@@ -45,14 +59,18 @@ class MontringPage extends ConsumerWidget {
                     : const SizedBox.shrink(),
               ),
             ),
-            const MontringTimer(),
-            Flexible(flex: 2, child: Container()),
-            const MontringCustomButton(),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.16,
-            ),
-          ],
-        ),
+          ),
+          const Positioned(
+            top: 160,
+            left: 74,
+            child: MontringTimer(),
+          ),
+          Positioned(
+            top: 490,
+            left: isRunning && dynamicSeconds != 0 ? 40 : 80,
+            child: const MontringCustomButton(),
+          ),
+        ],
       ),
     );
   }
