9d66eb6b88b12f0c9c9ebdd6bf2c77d753f487e4
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-10-26 21:58:19 +0900

    feat: アプリの利用規約とプライバシーポリシーのURL追加

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,2 +1,6 @@
 const kBaseUrl = 'http://localhost:8000';
 const kHealthCareAppUrl = 'x-apple-health://';
+
+const kAppTermsUrl = 'https://kiichi7580.github.io/serene_track_tos/';
+const kAppPrivacyPolicyUrl =
+    'https://kiichi7580.github.io/serene_track_privacy_policy/';
