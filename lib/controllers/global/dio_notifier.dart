8b43a0ea018ac1d96a9a3e68c68c131f550fa8a5
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-11-14 18:06:03 +0900

    fix: 細かい修正

--- lib/controllers/global/dio_notifier.dart
+++ lib/controllers/global/dio_notifier.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dioProvider = Provider<Dio>((ref) {
-  const baseUrl = String.fromEnvironment('baseUrl');
-  if (baseUrl.isEmpty) {
-    throw AssertionError('baseUrl is not set');
-  }
-  return Dio(
-    BaseOptions(
-      baseUrl: baseUrl,
-      connectTimeout: const Duration(seconds: 3),
-      receiveTimeout: const Duration(seconds: 4),
-    ),
-  );
-});
+final dioProvider = Provider<Dio>(
+  (ref) {
+    const baseUrl = String.fromEnvironment('baseUrl');
+    if (baseUrl.isEmpty) {
+      throw AssertionError('baseUrl is not set');
+    }
+    return Dio(
+      BaseOptions(
+        baseUrl: baseUrl,
+        connectTimeout: const Duration(seconds: 3),
+        receiveTimeout: const Duration(seconds: 4),
+      ),
+    );
+  },
+);
